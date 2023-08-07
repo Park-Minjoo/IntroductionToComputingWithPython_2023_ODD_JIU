1b069412635e4a1da35ce467256a73ed231d8238
Park-Minjoo <alswn9030@gmail.com>
2023-08-07 14:07:55 +0700

    [UPDATE] ICP Example 5 & 6

---
+++ ICP02/1.IntegerExample06.py
@@ -0,0 +1,8 @@
+price = input("Enter the price: ") # input value = 3000
+print(price)     # 3000
+print(price * 4) # 3000300030003000
+
+price = int(price)
+print(price)     # 3000
+print(price * 4) # 12000
+
