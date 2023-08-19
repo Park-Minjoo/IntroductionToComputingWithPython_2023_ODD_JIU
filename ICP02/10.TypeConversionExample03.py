f511107a2cacf4ee0e8da8391ff0be3b92043cee
Park-Minjoo <alswn9030@gmail.com>
2023-08-19 15:00:41 +0700

    [UPDATE] TypeConversionExample & Practice

---
+++ ICP02/10.TypeConversionExample03.py
@@ -0,0 +1,11 @@
+a = int('2')
+print(a) # 2
+
+b = float('1.5')
+print(b) # 1.5
+
+c = str(3)
+print(c) # 3
+
+d = int('2.5')
+print(d) # Q1.
---
+++ ICP02/10.TypeConversionPractice04.py
@@ -0,0 +1,3 @@
+number1 = float(input('Enter the first floating-point number: '))
+number2 = float(input('Enter the second floating-point number: '))
+print(number1 + number2)
