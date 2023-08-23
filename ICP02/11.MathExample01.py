f0b3d1067276486cc713c64a7d3ca1eac8b6bbbe
Park-Minjoo <alswn9030@gmail.com>
2023-08-23 19:48:01 +0700

    [UPDATE] ICP01

---
+++ ICP02/11.MathExample01.py
@@ -0,0 +1,23 @@
+import math
+
+# factorial()
+print(math.factorial(4)) # 24
+print(math.factorial(6)) # 720
+
+# floor()
+print(math.floor(8.3)) # 8
+print(math.floor(9.8)) # 9
+
+# greatest common divisor
+print(math.gcd(6, 3)) # 3
+
+# least common multiple
+print(math.lcm(3, 6)) # 6
+print(math.lcm(5, 10)) # 10
+
+# prod()
+a = [1, 3, 5]
+print(math.prod(a)) # 15
+
+# log()
+print(math.log(9)) # 2.1972245773362196
