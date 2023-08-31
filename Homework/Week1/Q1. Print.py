4fcadeac7ce6c9add67e82484d79c90e2cdef630
Park-Minjoo <alswn9030@gmail.com>
2023-08-31 08:51:29 +0700

    [UPDATE] Homework

--- Homework/Week1/Q1. Print.py
+++ Homework/Week1/Q1. Print.py
@@ -1,8 +1,8 @@
 # 1. Fill out the variables with your information
 # ex) name = "Amy", age = 20, major = "Information System"
-name = "Minjoo"
-age = 20
-major = "CS"
+name = "name"
+age = 18
+major = "Information Technology / Information System"
 
 # 2. Print the information, one by one.
 print(name)
