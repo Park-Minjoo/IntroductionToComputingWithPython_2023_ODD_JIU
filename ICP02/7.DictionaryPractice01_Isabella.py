a3be018daad1700ec4cf69846b209899272fc7c2
Park-Minjoo <alswn9030@gmail.com>
2023-09-04 14:14:55 +0700

    [UPDATE] Dictionary Practice by Isabella, Julior

---
+++ ICP02/7.DictionaryPractice01_Isabella.py
@@ -0,0 +1,6 @@
+birthdate = {}
+name = input("enter your name:")
+birthday = input("enter your birthdate:")
+
+birthdate = {name:birthday}
+print(birthdate)
---
+++ ICP02/7.DictionaryPractice02_Julior.py
@@ -0,0 +1,8 @@
+info = {}
+name = input("enter your name =")
+age = input("enter your age =")
+address = input("enter your address =")
+major = input("enter your major =")
+
+info = { 'Name' : name, 'Age': age, 'Address': address, 'Major': major}
+print(info)
