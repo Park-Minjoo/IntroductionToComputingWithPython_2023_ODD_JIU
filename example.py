9de8e371cb1412c7f9b52b88c3f70dca4f19eb76
Park-Minjoo <alswn9030@gmail.com>
2023-09-18 11:05:42 +0700

    [UPDATE] Some changes

--- example.py
+++ example.py
@@ -1,2 +1,7 @@
-num = int(input('Enter the number: '))
-print('*' * num)
+
+input_str = input("Enter two integers separated by a space: ")
+
+input_list = input_str.split()
+a = int(input_list[0])
+b = int(input_list[1])
+print("Result:", a - b)
